e0a49696eceb958036e37d49e55a0c4b4f740aaa
Joel Gurivireddy <joelpreetam@gmail.com>
2026-01-06 20:22:59 -0600

    Added sinusoidal positional encodings as well as some files in the layers directory

---
+++ layers/pe.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+
+class SinusoidalPE(nn.Module):
+    def __init__(self, seq_len, d_model):
+        super(SinusoidalPE, self).__init__()
+        self.pos = torch.arange(seq_len).unsqueeze(1).expand(-1, d_model).unsqueeze(0) # (N, d_model)
+        self.div = 10000 ** (2 * torch.arange(end=seq_len, step=2).unsqueeze(0).expand(seq_len, -1) / d_model) # (N, d_model / 2)
+        self.sin = torch.sin(self.pos[:, ::2] / self.div)
+        self.cos = torch.cos(self.pos[:, 1::2] / self.div)
+
+    def forward(self, X: torch.Tensor):
+        X[:, :, ::2] += self.sin
+        X[:, :, 1::2] += self.cos
+        return X # (B, N, d_model)
+    
+
+class RoPE(nn.Module):
+    def __init__(self, seq_len, d_model):
+        super(RoPE, self).__init__()
+
+    def forward(self, X: torch.Tensor):
+        pass
+
+
+class DecoupledRoPE(nn.Module):
+    def __init__(self, seq_len, d_model):
+        super(DecoupledRoPE, self).__init__()
+
+    def forward(self, X: torch.Tensor):
+        pass
+
+
+class ALiBi(nn.Module):
+    def __init__(self, seq_len, d_model):
+        super(ALiBi, self).__init__()
+
+    def forward(self, X: torch.Tensor):
+        pass
